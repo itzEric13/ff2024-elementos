d9f5e63f1005228c15f4770530e0aaf5f4e76df4
itzEric13 <eric.rsantos5@senacsp.edu.br>
2024-09-30 19:43:02 -0300

    feat: Capturando element por ID

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Selecionando o div pelo ID
+let div = 
+    document.getElementById('meuDiv');
+
+// Selecionando todos os paragrafos com a classe "Paragráfo"
+let paragrafos =
+    document.getElementsByClassName('paragrafo');
+
+//selecionando o primeiro parágrafo com a classe "parágrafo"
+let primeiroParagrafo = 
+    document.querySelector('.paragrafo');
+
+//Selcionando todos os parágrafos  dentro da div
+let paragrafosDentroDiv = 
+    document.querySelectorAll('#meuDiv p');
