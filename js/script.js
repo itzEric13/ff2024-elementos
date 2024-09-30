cf9400c11daeb09e6813e79ae6b2be7233f696c6
itzEric13 <eric.rsantos5@senacsp.edu.br>
2024-09-30 19:51:33 -0300

    feat: capturando primeiro paragráfo e todos os paragrafos

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 // Selecionando o div pelo ID
-let div = 
-    document.getElementById('meuDiv');
+let div = document.getElementById('meuDiv');
+console.log(meuDiv);// verificando se está funcionando ou não
 
 // Selecionando todos os paragrafos com a classe "Paragráfo"
-let paragrafos =
-    document.getElementsByClassName('paragrafo');
+let paragrafos = document.getElementsByClassName('paragrafo');
+console.log(paragrafos);// verificando se está funcionando ou não
 
 //selecionando o primeiro parágrafo com a classe "parágrafo"
-let primeiroParagrafo = 
-    document.querySelector('.paragrafo');
+let primeiroParagrafo = document.querySelector('.paragrafo');
+console.log(primeiroParagrafo);// verificando se está funcionando ou não
 
 //Selcionando todos os parágrafos  dentro da div
-let paragrafosDentroDiv = 
-    document.querySelectorAll('#meuDiv p');
+let todosParagrafos = document.querySelectorAll('#meuDiv p');
+console.log (todosParagrafos);// verificando se está funcionando ou não
+
