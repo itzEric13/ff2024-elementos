50b39e471235177f7c2340f36a5750ec935ccfd8
itzEric13 <eric.rsantos5@senacsp.edu.br>
2024-09-30 22:00:04 -0300

    feat: Ex 2 Corrigido

--- exercicios/ex2/js/script.js
+++ exercicios/ex2/js/script.js
@@ -1,8 +1,16 @@
-function aoClicar() {
-    let campo = document.querySelector('#meuCampo');
-    let valorDigitado = campo+value;
-    alert ('Você clicou e está escrito "' + valorDigitado + '"');
-}
+// crie dois inputs de tipo numero que ao clicar no botão somar, informe no alert os valores somados 
 
-let botao = document.querySelector('.btnExibirValor');
-botao.addEventListener('click', aoClicar);
+function somar () {
+    let numero1 = document.querySelector('.num1').value;
+    let numero2 = document.querySelector('.num2').value;
+
+    numero1 = parseInt(numero1);
+    numero2 = parseInt(numero2);
+    let soma = numero1 + numero2;
+
+    pResutado = document.querySelector("#resultado");
+    pResutado.innerHTML = `${numero1} + ${numero2} = ${soma}`;
+
+}
+let botao = document.querySelector("button");
+botao.addEventListener("click", somar);
