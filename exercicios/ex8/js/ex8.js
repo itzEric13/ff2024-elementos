55c8e30a0d91dee515ad55e7ccfd625eb570f9a5
itzEric13 <eric.rsantos5@senacsp.edu.br>
2024-10-01 22:28:25 -0300

    feat: finalizado

--- exercicios/ex8/js/ex8.js
+++ exercicios/ex8/js/ex8.js
@@ -1,13 +1,22 @@
-let nome = (prompt("Digite o seu nome:"));
-        let salarioHora = parseFloat(prompt("Qual é o seu salário por hora ? "));
-        let horaTrabalhada = parseFloat(prompt("Quantas horas você trabalha por mês ?"));
- 
-        let salario = salarioHora*horaTrabalhada;
-   
-       
- 
-    document.write("<br> <b>Nome: "+nome);
-    document.write("<br><b>Salário por hora: "+salarioHora);
-    document.write("<br><b>Horas trabalhadas no mês: "+horaTrabalhada);    
-    document.write("<br><b>Salário mensal:" +salario);
-    
+function calcularResultado() {
+
+
+    let nome = document.querySelector('#nome').value;
+
+    let valorHora =
+        parseFloat(document.querySelector('#valorHora').value);
+
+    let quantHora =
+        parseFloat(document.querySelector('#quantHora').value);
+
+    let resultado = valorHora * quantHora;
+
+
+
+    document.querySelector("#resultado").innerHTML =
+        `O valor do salário do(a) ${nome} é ${resultado.toFixed(2)}`;
+
+
+}
+
+document.querySelector('.btn').addEventListener('click', calcularResultado);
