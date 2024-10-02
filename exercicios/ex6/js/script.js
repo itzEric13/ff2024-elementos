77dd6ffa3a9a18a4bd32d69f9c4f5c252b6a8a75
itzEric13 <eric.rsantos5@senacsp.edu.br>
2024-10-01 21:45:25 -0300

    ex.corrigido

--- exercicios/ex6/js/script.js
+++ exercicios/ex6/js/script.js
@@ -5,11 +5,11 @@ function tresNumeros(){
 
 let maior ;
 
-if (num1 === num2 && num2 === num3) {
+if ((num1 === num2) && (num2 === num3)) {
     maior = "Os três números são iguais: " + num1;
-} else if (num1 >= num2 && num1 >= num3) {
+} else if ((num1 >= num2) && (num1 >= num3)) {
     maior = "primeiro número digitado: " + num1;
-} else if (num2 >= num1 && num2 >= num3) {
+} else if ((num2 >= num1) && (num2 >= num3)) {
     maior = "segundo número digitado: " + num2;
 } else {
     maior = "terceiro número digitado: " + num3;
