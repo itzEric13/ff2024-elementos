68b81d3873761c6d281161c0517c6ba4863a8f68
itzEric13 <eric.rsantos5@senacsp.edu.br>
2024-10-01 21:55:17 -0300

    feat: ex7 corrigido

--- exercicios/ex7/js/script.js
+++ exercicios/ex7/js/script.js
@@ -14,18 +14,18 @@ function maiorNumero(){
         } else {}
  
         
-            if (num1 >= num2 && num1 >= num3) {
+            if ((num1 >= num2) && (num1 >= num3)) {
                 maior = num1;
-            } else if (num2 >= num1 && num2 >= num3) {
+            } else if ((num2 >= num1) && (num2 >= num3)) {
                 maior = num2;
             } else {
                 maior = num3;
             }
  
            
-            if (num1 <= num2 && num1 <= num3) {
+            if ((num1 <= num2) && (num1 <= num3)) {
                 menor = num1;
-            } else if (num2 <= num1 && num2 <= num3) {
+            } else if ((num2 <= num1) && (num2 <= num3)) {
                 menor = num2;
             } else {
                 menor = num3;
@@ -34,3 +34,20 @@ function maiorNumero(){
  
         document.getElementById('resultado').innerText = `O maior é: ${maior} e o menor é: ${menor}`;
 }
+
+// Outro método
+// function exibirMaior() {
+//     let num1 = parseFloat(document.querySelector("#num1").value);
+//     let num2 = parseFloat(document.querySelector("#num2").value);
+//     let num3 = parseFloat(document.querySelector("#num3").value);
+//     let resultado = "";
+ 
+//     let maior = Math.max(num1, num2, num3);
+//     let menor = Math.min(num1, num2, num3);
+ 
+//     resultado =`${maior} é maior! <br/>`;
+//     resultado +=`${menor} é menor!`;
+//     document.querySelector("#resultado").innerHTML = resultado;
+// }
+ 
+// document.querySelector(".btn").addEventListener('click', exibirMaior);
